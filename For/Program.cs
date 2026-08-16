fd754e82df8b5445efeb978a5e74d3ab507a5202
FFFLAREEE <yzhaoci11@gmail.com>
2026-08-16 17:54:45 +1000

    practice

---
+++ For/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Forwhile;
+class Program
+{
+    static void Main()
+    {
+        int count = 0;
+        for (int i = 0; i < 10; i++)//执行完{}里的内容再执行i++
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine(count++);
+            }
+        }
+    }
+}
+//for主要用于有明确终止次数饿循环
---
+++ While/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace While;
+class Program
+{
+    static void Main()
+    {
+        while (true)//把读取用户输入的东西放进死循环是很常见的做法
+        // for (; ; ) this is also a infinite loop
+        //do{Console.WriteLine("1111")}while(false);  尽管是false 但是括号里的内容也会被执行一次
+        {
+            Console.WriteLine("pls input,press q to exit");
+            string input =Console.ReadLine();
+            if (input == "q")
+            {
+                Console.WriteLine("Break");
+                break;
+            }
+            else if (input == "c")
+            {
+                continue;//不管后面还有什么 continue就直接跳到新的循环
+                //continue and break can be used in any loop, not only for-while loop
+            }
+            Console.WriteLine("Your input is:"+input);
+        }
+    }
+}
+//while主要用于有明确终止条件的循环
