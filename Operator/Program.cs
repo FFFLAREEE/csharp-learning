8b8f22d51c15f8c5e2704c6ed475e3c2d1f9d278
FFFLAREEE <yzhaoci11@gmail.com>
2026-08-16 16:57:26 +1000

    Add Operator Program.cs

---
+++ Operator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpHelloWorld;
+
+class Program
+{
+    static int Foo()
+    {
+        bool b = true;
+        return b ? 1 : 2;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Foo());
+    }
+}
