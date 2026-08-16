99765625d1cbcfea84f53b37a88256dcee4d7a2e
FFFLAREEE <yzhaoci11@gmail.com>
2026-08-16 18:02:38 +1000

    2

---
+++ homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace homework1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 1;
+            for (int i = 1; i < 10; i++) 
+            {
+                for (int j = i; j < 10; j++)
+                {
+                    Console.WriteLine(i+"*"+j+"="+i*j);
+                }
+            }
+        }
+    }
+}
+    
