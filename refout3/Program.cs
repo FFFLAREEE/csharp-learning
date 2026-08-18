bd4474196c8ef1a792380a351a218d9054b11d49
FFFLAREEE <yzhaoci11@gmail.com>
2026-08-18 17:34:48 +1000

    0

--- refout3/Program.cs
+++ refout3/Program.cs
@@ -3,6 +3,7 @@
     class Program
     {
         static void Mylog(string msg, int level=0)
+        //一旦第一个默认参数出现了 后面的所有参数都应该是默认参数
         {
             Console.WriteLine($"[{level}] {msg}");
         }
