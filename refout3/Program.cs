3ef9b58f0b330726098f050f87408eec6f49b361
FFFLAREEE <yzhaoci11@gmail.com>
2026-08-18 17:32:36 +1000

    1

---
+++ refout3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace  refout3
+{
+    class Program
+    {
+        static void Mylog(string msg, int level)
+        {
+            Console.WriteLine($"[{level}] {msg}");
+        }
+
+        static void Main()
+        {
+            Mylog("Hello",0);
+        }
+    }
+}
