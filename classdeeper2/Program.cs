3a8faad408ac1ccdcb4b74b13f1039eb2d26ec5b
FFFLAREEE <yzhaoci11@gmail.com>
2026-08-18 19:13:24 +1000

    0

---
+++ classdeeper2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace  classdeeper2
+{
+    class Student
+    {
+        public Student(int age,string name)// ctor 然后tab 自动出来一个没有参数的构造函数
+        {
+            Age = age;
+            Name= name;
+
+        }
+
+        private int _age;
+        public string Name{get;init ;}// init 和set 的区别在 init只能在构造的阶段用 初始化成功后再也不能修改相应的属性了
+        // private set 就是可以在类内修改 不能在类外修改
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 100)
+                {
+                    throw new ArgumentOutOfRangeException("age", "age must be less than 100");
+                }
+                _age = value;
+            }
+        }
+        
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"My name is {Name},and I am {Age} years old");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Student stu1 =new Student(17,"Jerry");
+           
+            stu1.IntroduceMyself();
+        }
+    }
+}
---
+++ classdeeper3/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace  classdeeper3
+{
+    class Student
+    {
+        public Student(string name)// ctor 然后tab 自动出来一个没有参数的构造函数
+        {
+           
+            Name= name;
+
+        }
+
+        private int _age;
+        public string Name{get;init ;}// init 和set 的区别在 
+        //init 可以在构造函数中赋值，也可以在对象初始化器中赋值；对象初始化完成以后，不能再次赋值
+        // private set 就是可以在类内修改 不能在类外修改
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 100)
+                {
+                    throw new ArgumentOutOfRangeException("age", "age must be less than 100");
+                }
+                _age = value;
+            }
+        }
+        
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"My name is {Name},and I am {Age} years old");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Student tom = new Student("Tom")
+            {
+                Age = 1,
+                Name = "123"//对象初始化语句执行顺序是在构造函数之后的
+            };
+           
+            tom.IntroduceMyself();
+        }
+    }
+}
